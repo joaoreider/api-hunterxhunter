5addb7a64b5ccc40d7e4b73c504db94859e3328f
joaoreider <joaopauloj405@gmail.com>
2023-04-04 16:05:27 -0300

    feat(personagem): add personagem endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PersonagensModule } from './personagens/personagens.module';
+import { PersonagemModule } from './personagem/personagem.module';
 
 @Module({
-  imports: [],
+  imports: [PersonagensModule, PersonagemModule],
   controllers: [AppController],
   providers: [AppService],
 })
