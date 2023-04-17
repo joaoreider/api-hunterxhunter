3798cceec90de2866f0807d99d142b5e70972ef8
joaoreider <joaopauloj405@gmail.com>
2023-04-17 14:38:57 -0300

    feat(auth): create module, service, controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PersonagemModule } from './personagem/personagem.module';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 
 @Module({
-  imports: [PersonagemModule, UserModule],
+  imports: [PersonagemModule, UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
