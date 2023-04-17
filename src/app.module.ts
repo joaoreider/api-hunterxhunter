6e895889af1dcc6e4498466845f9e36e168476f1
joaoreider <joaopauloj405@gmail.com>
2023-04-16 21:40:37 -0300

    feat(user): generate resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PersonagemModule } from './personagem/personagem.module';
+import { UserModule } from './user/user.module';
 
 
 @Module({
-  imports: [PersonagemModule],
+  imports: [PersonagemModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
